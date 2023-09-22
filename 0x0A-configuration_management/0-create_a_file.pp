83a167491f26d265b0feb87b438301b2a1412a3a
Levlav <levinlavatsa.com>
2023-09-22 12:20:08 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-Create file called school
-file { '/tmp/school':
-  ensure  => 'present',
+# create a file in /tmp with specific requirements
+
+file { 'school':
+  path    => '/tmp/school',
   mode    => '0744',
-  owner   => www-data,
-  group   => www-data,
-  content => 'I love Puppet'
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
